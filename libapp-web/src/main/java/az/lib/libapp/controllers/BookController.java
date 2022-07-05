3c95139b359140d39e4efc48e0e54d28cfae01fd
v-eyvazov <eyvazovvusal.1999@gmail.com>
2022-07-05 17:49:38 +0400

    Refactored from one-module to multi-module app.

--- libapp-web/src/main/java/az/lib/libapp/controllers/BookController.java
+++ libapp-web/src/main/java/az/lib/libapp/controllers/BookController.java
@@ -13,11 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 
 import static az.lib.libapp.utilities.Extract.extractId;
-
 
 @Controller
 public class BookController {
--- libapp-web/src/main/java/az/lib/libapp/dto/forms/BookForm.java
+++ libapp-web/src/main/java/az/lib/libapp/dto/forms/BookForm.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
